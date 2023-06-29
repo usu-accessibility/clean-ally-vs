02899d11a4987a958806edc0ff5dc2e7ed9d3bff
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-06-29 17:38:41 -0600

    integrated with lambda service

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# clean-canvas README
+# clean-ally-vs README
 
 Cleans extraneous HTML that is generated from the Anthology Ally HTML export.
 
 ## Features
 
 Steps:-
+
 * Install extension “clean-ally”.
 * Select all the HTML Code and run the command “start clean-ally” using “shift + command + p”.
+* Select the Start Clean-ally option from explorer menu by right clicking on the file tab to run the command
+* Select the Start Clean-ally option from navigation menu.
 * Cleans out the extraneous HTML.
 
 Note:- 
-* We can add keyboard shortcut to execute "start clean-ally" command at preferences -> keyboard shorcuts -> Add shortcut key for "start clean-ally".  
+* We can add keyboard shortcut to execute "Start Clean-ally" command at preferences -> keyboard shorcuts -> Add shortcut key for "Start Clean-ally".  
--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const {getParsedPTagCode} = require('./remove-p-tag');
-
+const axios = require('axios');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,71 +13,46 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "clean-ally" is now active!');
+	console.log('Congratulations, your extension "clean-ally-vs" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clean-ally.cleanAlly', function () {
+	let disposable = vscode.commands.registerCommand('clean-ally-vs.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
+
 		// Get the current text editor
-		let editor = vscode.window.activeTextEditor; 
-		const selection = editor.selection
-		let htmlCodeText  = editor.document.getText(selection);
+        let editor = vscode.window.activeTextEditor; 
+        const selection = editor.selection
+        let htmlCodeText  = editor.document.getText(selection);
 
-		// Search and replace happens below
+		if(htmlCodeText === ""){
+			const document = editor.document;
+			const entireRange = new vscode.Range(0, 0, document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+			const entireSelection = new vscode.Selection(entireRange.start, entireRange.end);
+			editor.selection = entireSelection;
 
-		// THINGS TO REMOVE
+			htmlCodeText = editor.document.getText(entireSelection);
+		}
 
-		const searchLang = /.lang="....."/g;
-		const replaceLang = "";
+		const apiUrl = "https://iq33vfcxl8.execute-api.us-east-1.amazonaws.com/default/parseHtml";
 
-		const searchDir = /.dir="..."/g;
-		const replaceDir = "";
+		const data = {
+			html: htmlCodeText,
+			access_token: "enter the access token",
+			mode: 'clean_ally_for_canvas'
+		};
 
-		const searchStart = /.start="1"/g;
-		const replaceStart = "";
-
-		const searchType = /.type="1"/g;
-		const replaceType = "";
-
-		const searchDiv = /<div>/g;
-		const replaceDiv = "";
-
-		const searchSpan = /<span( |.)*?>/g;
-		const replaceSpan = "";
-
-		const searchSpan2 = /<\/span>/g;
-		const replaceSpan2 = "";
-
-		const searchDiv2 = /\/div>/g;
-		const replaceDiv2 = "";
-
-		// THINGS TO REPLACE
-
-		const searchFigure = /<figure>(.|\s)*?<\/figure>/g;
-		const replaceFigure = "[IMAGE]";
-
-		const searchStyle = /<style( |.)*?>(.|\n)*?<\/style>/g;
-		const replaceStyle = "<style></style>";
-
-		// REPLACEMENT FOR REMOVES
-		htmlCodeText = htmlCodeText.replace(searchLang, replaceLang);
-		htmlCodeText = htmlCodeText.replace(searchDir, replaceDir);
-		htmlCodeText = htmlCodeText.replace(searchStart, replaceStart);
-		htmlCodeText = htmlCodeText.replace(searchType, replaceType);
-		htmlCodeText = htmlCodeText.replace(searchDiv, replaceDiv);
-		htmlCodeText = htmlCodeText.replace(searchSpan, replaceSpan);
-		htmlCodeText = htmlCodeText.replace(searchSpan2, replaceSpan2);
-		htmlCodeText = htmlCodeText.replace(searchDiv2, replaceDiv2);
-		
-		// REPLACEMENT FOR REPLACES
-		htmlCodeText = htmlCodeText.replace(searchFigure, replaceFigure);
-		htmlCodeText = htmlCodeText.replace(searchStyle, replaceStyle);
-		
-		editor.edit(builder => builder.replace(selection, getParsedPTagCode(htmlCodeText)));
-		// Display a message box to the user
-		vscode.window.showInformationMessage("Cleaned out extraneous HTML successfully");
+		axios.post(apiUrl, data)
+			.then(response => {
+				htmlCodeText = response.data;
+				editor.edit(builder => builder.replace(selection, htmlCodeText));
+				// Display a message box to the user
+				vscode.window.showInformationMessage('Cleaned out extraneous HTML successfully');
+			})
+			.catch(error => {
+				console.error(error);
+			});
 	});
 
 	context.subscriptions.push(disposable);
